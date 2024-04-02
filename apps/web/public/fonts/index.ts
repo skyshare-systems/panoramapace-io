8ddb184df2b7ad7d57bdb397e01cca181ed2345b
Binz <sikatunar@gmail.com>
2024-04-02 22:57:44 +0800

    init

---
+++ apps/web/public/fonts/index.ts
@@ -0,0 +1,5 @@
+import localFont from "next/font/local";
+
+export const swansea = localFont({
+  src: "./local/aeonik-regular.otf",
+});
--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,20 +1,98 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./views/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      screens: {
+        xsm: "375px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "custom-xl": "1399px",
+        "2xl": "1536px",
+        "3xl": "1920px",
+      },
+
+      colors: {
+        light_blue:{
+          100:"#2C4BF9",
+          80:"rgba(44, 75, 249, 0.80)",
+          72:"rgba(44, 75, 249, 0.72)",
+          50:"rgba(44, 75, 249, 0.50)",
+          32:"rgba(44, 75, 249, 0.32)",
+          16:"rgba(44, 75, 249, 0.16)",
+          12:"rgba(44, 75, 249, 0.12)",
+          8:"rgba(44, 75, 249, 0.08)",
+          4:"rgba(44, 75, 249, 0.04)"
+        },
+
+        dark_blue:{
+          100:"#00315C",
+          80:"rgba(0, 49, 92, 0.80)",
+          72:"rgba(0, 49, 92, 0.72)",
+          50:"rgba(0, 49, 92, 0.50)",
+          32:"rgba(0, 49, 92, 0.32)",
+          16:"rgba(0, 49, 92, 0.16)",
+          12:"rgba(0, 49, 92, 0.12)",
+          8:"rgba(0, 49, 92, 0.08)",
+          4:"rgba(0, 49, 92, 0.04)"
+        },
+
+        white:{
+          100:"#FEFEFE",
+          80:"rgba(254, 254, 254, 0.80)",
+          72:"rgba(254, 254, 254, 0.72)",
+          50:"rgba(254, 254, 254, 0.50)",
+          32:"rgba(254, 254, 254, 0.32)",
+          16:"rgba(254, 254, 254, 0.16)",
+          12:"rgba(254, 254, 254, 0.12)",
+          8:"rgba(254, 254, 254, 0.08)",
+          4:"rgba(254, 254, 254, 0.04)"
+        },
+
+        black:{
+          100:"#000000",
+          80:"rgba(20, 20, 20, 0.80)",
+          72:"rgba(20, 20, 20, 0.72)",
+          50:"rgba(20, 20, 20, 0.50)",
+          32:"rgba(20, 20, 20, 0.32)",
+          16:"rgba(20, 20, 20, 0.16)",
+          12:"rgba(20, 20, 20, 0.12)",
+          8:"rgba(20, 20, 20, 0.08)",
+          4:"rgba(20, 20, 20, 0.04)"
+        },
+
+        dark_orange:{
+          100:"#DB5000",
+          80:"rgba(219, 80, 0, 0.80)",
+          72:"rgba(219, 80, 0, 0.72)",
+          50:"rgba(219, 80, 0, 0.50)",
+          32:"rgba(219, 80, 0, 0.32)",
+          16:"rgba(219, 80, 0, 0.16)",
+          12:"rgba(219, 80, 0, 0.12)",
+          8:"rgba(219, 80, 0, 0.08)",
+          4:"rgba(219, 80, 0, 0.04)"
+        },
+
+        light_orange:{
+          100:"#FF7700",
+          80:"rgba(255, 119, 0, 0.80)",
+          72:"rgba(255, 119, 0, 0.72)",
+          50:"rgba(255, 119, 0, 0.50)",
+          32:"rgba(255, 119, 0, 0.32)",
+          16:"rgba(255, 119, 0, 0.16)",
+          12:"rgba(255, 119, 0, 0.12)",
+          8:"rgba(255, 119, 0, 0.08)",
+          4:"rgba(255, 119, 0, 0.04)"
+        }
       },
     },
   },
   plugins: [],
 };
-export default config;
