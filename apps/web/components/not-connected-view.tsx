b21f173ed0be9bf3cb54a687625c9a2e6b03fd2e
Binz <sikatunar@gmail.com>
2024-04-04 14:48:38 +0800

    add dashboard view

---
+++ apps/web/components/not-connected-view.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import LineBg from "./icons/bg-line";
+import { cn } from "@/lib/utils";
+import { aoenik_regular } from "@/public/fonts";
+import { ConnectWallet } from "./connect-wallet";
+
+const NotConnectedView = () => {
+  return (
+    <div className="flex justify-center items-center relative">
+      <LineBg className="absolute -top-[7rem] w-full -z-[1]" />
+      <div className="flex flex-col items-center justify-center max-w-[1200px] gap-8 lg:px-0 px-4 pb-[8rem] pt-[5rem]">
+        <div className="flex flex-col gap-4 items-center justify-center">
+          <h1
+            className={cn(
+              aoenik_regular.className,
+              "ty-h3 lg:ty-display text-white-100 text-center"
+            )}
+          >
+            Start creating your personalized portfolio
+          </h1>
+          <p
+            className={cn(
+              aoenik_regular.className,
+              "ty-subheading text-white-50 text-center"
+            )}
+          >
+            Track your Web3 investment across 60+ chains and CEX in one single
+            place
+          </p>
+        </div>
+        <ConnectWallet />
+      </div>
+    </div>
+  );
+};
+
+export default NotConnectedView;
---
+++ apps/web/views/dashboard/DashboardPage.tsx
@@ -0,0 +1,44 @@
+import CeilingBg from "@/components/icons/bg-ceiling";
+import { cn } from "@/lib/utils";
+import { aoenik_regular } from "@/public/fonts";
+import React from "react";
+import ListWallet from "./ListWallet";
+import PortfolioPage from "../portfolio/PortfolioPage";
+
+const DashboardPage = () => {
+  return (
+    <div className="flex justify-center items-center py-[5rem] relative">
+      <CeilingBg className="absolute -top-[7rem] w-full -z-[1] opacity-40" />
+      <div className="flex flex-col max-w-[1200px] gap-16 grow items-start w-full">
+        {/* Dashboard */}
+        <div className="flex flex-col gap-8 items-start justify-start grow w-full">
+          <div className="flex flex-col gap-2">
+            <h1
+              className={cn(
+                aoenik_regular.className,
+                "tyh3 lg:ty-h1 text-white-100 leading-none"
+              )}
+            >
+              Dashboard
+            </h1>
+            <p
+              className={cn(
+                aoenik_regular.className,
+                "ty-subheading text-white-50"
+              )}
+            >
+              See your summary of your portfolio
+            </p>
+          </div>
+
+          {/* Add Wallet  */}
+          <ListWallet />
+        </div>
+        {/* Portfolio */}
+        <PortfolioPage />
+      </div>
+    </div>
+  );
+};
+
+export default DashboardPage;
