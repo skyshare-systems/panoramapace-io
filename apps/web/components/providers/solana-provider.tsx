7b26a35e50cb4c7c4212c0a8874e01d96b8560d9
BinZ <sikatunar@gmail.com>
2024-04-07 21:56:28 +0800

    add packages

--- apps/web/components/providers/solana-provider.tsx
+++ apps/web/components/providers/solana-provider.tsx
@@ -1,51 +1,24 @@
 "use client";
-// import {
-//   createDefaultAuthorizationResultCache,
-//   SolanaMobileWalletAdapter,
-// } from "@solana-mobile/wallet-adapter-mobile";
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import {
   ConnectionProvider,
   WalletProvider,
 } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
-import { clusterApiUrl } from "@solana/web3.js";
-import { AppProps } from "next/app";
-import { FC, useMemo } from "react";
-
-// Use require instead of import since order matters
+import * as walletAdapterWallets from "@solana/wallet-adapter-wallets";
+import * as web3 from "@solana/web3.js";
 require("@solana/wallet-adapter-react-ui/styles.css");
-// require("../styles/globals.css");
 
-const SolanaProvider = ({ children }: any) => {
-  // Can be set to 'devnet', 'testnet', or 'mainnet-beta'
-  const network = "http://127.0.0.1:8899";
-
-  // You can also provide a custom RPC endpoint
-  const endpoint = useMemo(() => network, [network]);
-
-  // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
-  // Only the wallets you configure here will be compiled into your application, and only the dependencies
-  // of wallets that your users connect to will be loaded
-  const wallets = useMemo(
-    () => [
-      //   new SolanaMobileWalletAdapter({
-      //     appIdentity: { name: "Solana Next.js Starter App" },
-      //     authorizationResultCache: createDefaultAuthorizationResultCache(),
-      //   }),
-      new PhantomWalletAdapter(),
-    ],
-    [network]
-  );
+const WalletContextProvider = ({ children }: any) => {
+  const endpoint = web3.clusterApiUrl("devnet");
+  const wallets = [new walletAdapterWallets.PhantomWalletAdapter()];
 
   return (
     <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={wallets} autoConnect>
+      <WalletProvider wallets={wallets}>
         <WalletModalProvider>{children}</WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
   );
 };
 
-export default SolanaProvider;
+export default WalletContextProvider;
